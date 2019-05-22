f5da1de7b18c975c73a53fe7a7862406052ef758
sasuke5055 <maciek.lewandowicz@gmail.com>
2019-05-22 14:23:07 +0200

    Cośtam nowego

--- src/Test.java
+++ src/Test.java
@@ -7,7 +7,6 @@ public class Test {
     public static void main(String[] args) throws IOException, ClassNotFoundException  {
         EventQueue.invokeLater(new Runnable() {
 
-            @Override
             public void run() {
                 new Wilk();
             }
--- src/Wilk.java
+++ src/Wilk.java
@@ -8,15 +8,61 @@ import java.util.ArrayList;
 public class Wilk extends JFrame implements ActionListener {
 
     private int startingNumberOfRabbits=0;
+    private int xSize=0;
+    private int ySize=0;
+    private double k;
     private Wolf wolf;
     private ArrayList<Rabbit> zajacList;
+
+    private JTextField giveK;
+    private JTextField giveX;
+    private JTextField giveY;
+    private JTextField giveRabbitsNumber;
+
+    private JTextArea podajK;
+    private JTextArea podajX;
+    private JTextArea podajY;
+    private JTextArea podajRabbitsNumber;
+
+    private JButton start;
 
     public Wilk(){
         super("Symulacja");
         setLayout(null);
+        setSize(900,700);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         zajacList = new ArrayList<Rabbit>();
         wolf = new Wolf();
 
+        giveK = new JTextField();
+        giveX = new JTextField();
+        giveY= new JTextField();
+        giveRabbitsNumber= new JTextField();
+        podajK = new JTextArea();
+        podajX = new JTextArea();
+        podajY = new JTextArea();
+        podajRabbitsNumber = new JTextArea();
+        start = new JButton("Start");
+
+        add(giveK);
+        add(giveRabbitsNumber);
+        add(giveX);
+        add(giveY);
+        add(start);
+        add(podajK);
+        add(podajRabbitsNumber);
+        add(podajX);
+        add(podajY);
+
+        
+
+        {  //dodanie planszy
+            Plansza plansza = new Plansza(30, 30, 5);
+            add(plansza);
+            plansza.setBounds(0, 0, 600, 600);
+        }
 
     }
 
